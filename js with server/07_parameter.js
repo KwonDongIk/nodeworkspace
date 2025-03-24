d0d9311c11ff367fc36c330efc1d682882e39ee6
KwonDongIk <dongikik@kakao.com>
2025-03-24 17:42:36 +0900

    0324 PM 17:42 commit

---
+++ js with server/07_parameter.js
@@ -0,0 +1,29 @@
+function say(message){
+  let msg = (message == undefined ? '매개변수가 넘어오지 않았습니다.' : message);
+  console.log(msg);
+};
+
+say('hi');
+say();
+
+function msg(message = '매개변수가 넘어오지 않았습니다.'){
+  console.log(message);
+};
+
+msg('hello');
+msg();
+
+
+const plus = function(x, y, ...rests){
+  let result = x + y;
+  for(let rest of rests){
+    result += rest;
+  }
+  return result;
+}
+
+console.log(plus(1,2));
+console.log(plus(1,2,3,4,5));
+console.log(plus(1,2,3,4,5,6,7,8,9,10));
+
+
